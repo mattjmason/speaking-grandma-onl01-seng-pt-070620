fbe8398c16ed34897107439c1f0a9db1b4e706b4
Matt Mason <masonmj10@gmail.com>
2020-07-15 16:33:30 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,7 +17,7 @@ def speak_to_grandma(phrase)
 
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase != phrase.upcase
+  elsif phrase = phrase.downcase
    "HUH?! SPEAK UP, SONNY!"
   else phrase == phrase.upcase
    "NO, NOT SINCE 1938!"
